6be2c5c833702d7bc197e0ca1083fbe1d4c34004
kosovic-creator <drasko.kosovic@icloud.com>
2025-04-18 08:53:11 +0200

    id

--- app/artikli/[id]/page.tsx
+++ app/artikli/[id]/page.tsx
@@ -1,30 +1,30 @@
-import { getProductById } from "@/actions/product.actions";
+import { prikaziArtikalId } from "@/actions/artikli.actions";
 
 interface PageProps {
     params: Promise<{ id: string }>;
 }
 
-export default async function ProductPage({ params }: PageProps) {
+export default async function ArtikalId({ params }: PageProps) {
   const { id } =await params;
 
   try {
-    const product = await getProductById(Number(id));
+    const artikal = await prikaziArtikalId(Number(id));
 
-    if (!product) {
+    if (!artikal) {
       return (
         <div className="flex flex-col items-center justify-center min-h-screen">
-          <h1 className="text-3xl font-bold mb-6">Product Not Found</h1>
-          <p>No product found with ID {id}.</p>
+          <h1 className="text-3xl font-bold mb-6">PArtikal nije nađen</h1>
+          <p>Nema artikla sa unjetim ID {id}.</p>
         </div>
       );
     }
 
     return (
       <div className="flex flex-col items-center justify-center min-h-screen">
-        <h1 className="text-3xl font-bold mb-6">Product Details</h1>
+        <h1 className="text-3xl font-bold mb-6">Artikal</h1>
         <div className="bg-white shadow-md rounded-lg p-6 max-w-sm w-full">
-          <h2 className="text-xl font-semibold mb-4">{product.name}</h2>
-          <p className="text-gray-700">Price: ${product.price}</p>
+          <h2 className="text-xl font-semibold mb-4">{artikal.naziv}</h2>
+          <p className="text-gray-700">Cijena: €{artikal.cijena}</p>
         </div>
       </div>
     );
@@ -32,8 +32,8 @@ export default async function ProductPage({ params }: PageProps) {
     console.error("Error fetching product:", error);
     return (
       <div className="flex flex-col items-center justify-center min-h-screen">
-        <h1 className="text-3xl font-bold mb-6">Error</h1>
-        <p>Failed to fetch product details. Please try again later.</p>
+        <h1 className="text-3xl font-bold mb-6">Greška</h1>
+        <p>Greška pri učitavanju artikla, Probajte ponovo.</p>
       </div>
     );
   }
