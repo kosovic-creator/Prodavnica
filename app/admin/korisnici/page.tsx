fa366021afb7c5399b6388997c89519c57affd81
kosovic-creator <drasko.kosovic@icloud.com>
2025-04-18 23:36:21 +0200

    list

--- app/admin/korisnici/page.tsx
+++ app/admin/korisnici/page.tsx
@@ -1,6 +1,7 @@
  'use client';
 import React, { useEffect, useState } from 'react';
 import { getAllUsers } from "@/actions/index";
+import Link from 'next/link';
 
 const UserList: React.FC = () => {
   const [users, setUsers] = useState<any[]>([]);
@@ -23,7 +24,7 @@ const UserList: React.FC = () => {
   }, []);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <div>Učitavam...</div>;
   }
 
   if (error) {
@@ -31,15 +32,37 @@ const UserList: React.FC = () => {
   }
 
   return (
-    <div>
-      <h1>User List</h1>
-      <ul>
-        {users.map(user => (
-          <li key={user.id}>{user.name} - {user.username}</li>
-        ))}
-      </ul>
+
+    <div className="flex flex-col items-center justify-center min-h-screen">
+       <Link className='text-emerald-900' href="/admin/korisnici/novi">Dodaj</Link>
+      <h1 className="text-3xl font-bold mb-6">Lista Korisnika</h1>
+      <table className="min-w-full bg-white shadow-md rounded-lg overflow-hidden">
+        <thead>
+          <tr>
+            <th className="py-2 px-4 border-b">ID</th>
+            <th className="py-2 px-4 border-b">Ime i Prezime</th>
+            <th className="py-2 px-4 border-b">Korisničko Ime</th>
+            <th className="py-2 px-4 border-b"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user.id}>
+              <td className="py-2 px-4 border-b">{user.id}</td>
+              <td className="py-2 px-4 border-b">{user.name}</td>
+              <td className="py-2 px-4 border-b">{user.username}</td>
+              <td>
+                  <Link className='text-emerald-600' href={`/admin/korisnici/${user.id}/izmjeni`}>Prikaži</Link>
+                  <Link className='text-amber-700' href={`/admin/korisnici/${user.id}/ukloni`}>Ukloni</Link>
+                </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
+  //   <div className="flex flex-col items-center justify-center min-h-screen"></div>
+  //     <h1 className="text-3xl font-bold mb-6">Lista Korisnika</h1>
 };
 
 export default UserList;
